739dfd59c4f6f79c24b1e55de20f1b45d2397691
Natassa <112552395+natko22@users.noreply.github.com>
2023-05-11 21:14:13 +0200

    Feature validation (#1)
    * size changed
    
    * fix password validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -15,10 +15,20 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signup", async (req, res, next) => {
+  console.log(req.body.password);
   try {
     const foundUser = await User.findOne({ username: req.body.username });
     if (foundUser) {
       res.send("Username is already taken.");
+      return;
+    }
+
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(req.body.password)) {
+      res.status(500).render("auth/signup", {
+        errorMessage:
+          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      });
       return;
     }
 
