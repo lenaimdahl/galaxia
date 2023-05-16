3a0d38c7413dcd978758fb69650593b0c5717db9
Lena Imdahl <mail@lenaimdahl.de>
2023-05-16 21:53:58 +0200

    log dir name

--- config/index.js
+++ config/index.js
@@ -49,16 +49,15 @@ module.exports = (app) => {
   );
 
   // Normalizes the path to the views folder
-  app.set("views", path.join(__dirname, "..", "views"));
+  app.set("views", path.join(__dirname, "../views"));
+  console.log("__dirname", __dirname);
   // Sets the view engine to handlebars
   app.set("view engine", "hbs");
   // Handles access to the public folder
-  app.use(express.static(path.join(__dirname, "..", "public")));
+  app.use(express.static(path.join(__dirname, "../public")));
 
   // Handles access to the favicon
-  app.use(
-    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
-  );
+  app.use(favicon(path.join(__dirname, "../public/images/favicon.ico")));
 
   //helper for: check if user is logged in
   //!! create boolean from value, if it exists, its true
